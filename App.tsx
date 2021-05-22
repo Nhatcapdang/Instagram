672659e33002874441e501a75502f04dd3a392e0
Nguyễn Văn Kiệt <Vankiet.kn78@gmail.com>
2021-05-22 21:36:56 +0700

    initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {SafeAreaView} from 'react-native';
+import TabNavigation from './src/TabNavigation';
+
+export default function App() {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <TabNavigation />
+    </SafeAreaView>
+  );
+}
